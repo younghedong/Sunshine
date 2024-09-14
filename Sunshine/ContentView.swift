f54ef35c5bbc215eb9a308ea050b46ffeb101249
hedong <hedong@kuaishou.com>
2024-09-15 01:43:04 +0800

    Initial Commit

---
+++ Sunshine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sunshine
+//
+//  Created by dongzi on 2024/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sunshine/SunshineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunshineApp.swift
+//  Sunshine
+//
+//  Created by dongzi on 2024/9/15.
+//
+
+import SwiftUI
+
+@main
+struct SunshineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
